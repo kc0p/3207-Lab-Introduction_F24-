60044f9355a9ad915482276300f90b676aac0302
Kaitlyn <tup17805@temple.edu>
2024-08-29 17:08:01 -0400

    implement working standalone random.c

--- print_random.c
+++ print_random.c
@@ -1,11 +1,12 @@
 #include <stddef.h>
+//#include "random"
 
 //Given Project code
 char* rand_string_alloc(size_t size)
 {
      char *s = malloc(size + 1);
      if (s) {
-         rand_string(s, size);
+         randchar(s, size);
      }
      return s;
 }
